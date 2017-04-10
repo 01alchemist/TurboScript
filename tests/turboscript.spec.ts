b74e3a4661b75b2b21c3d02ae433fb498f098119
Wink Saville <wink@saville.com>
2017-04-10 11:54:29 -0700

    Remove instantiateFile and instantiateTbs methods.
    They are now in utils.
    
    Also simplify the tests and add jsdocs.

--- tests/turboscript.spec.ts
+++ tests/turboscript.spec.ts
@@ -1,70 +1,19 @@
 import * as debugModule from "debug";
 const debug = debugModule("turboscript.spec");
 
-import {readFileAsync, tcCompile, unlinkAsync} from "./utils";
+import { instantiateTbsFile } from "./utils";
 
 import {
-    SetupFixture,
-    Setup,
-    TeardownFixture,
-    Teardown,
     AsyncTest,
     Expect,
+    SetupFixture,
+    Setup,
+    Teardown,
+    TeardownFixture,
     Test,
     TestCase,
     TestFixture,
 } from "alsatian";
-
-async function instantiateFile(filePath: string): Promise<WebAssembly.Instance> {
-    debug(`instantiateFile:+ filePath=${filePath}`);
-
-    // Read the file
-    debug("instantiateFile: readFile:");
-    let result = await readFileAsync(filePath);
-    // debug(`instantiateFile: result=${JSON.stringify(result)}`);
-    debug(`instantiateFile: file read length=${result.data.length}`);
-
-    // Compile
-    debug("instantiateFile compile:");
-    let mod = await WebAssembly.compile(result.data);
-    debug("instantiateFile compiled:");
-
-    // Instantiate:
-    debug("instantiateFile instantiate:");
-    let instance = await WebAssembly.instantiate(mod);
-    debug("instantiateFile instantiated:");
-
-    debug(`instantiateFile:- filePath=${filePath} instance=${instance}`);
-    return instance;
-}
-
-async function instantiateTbsFile(filePath: string): Promise<WebAssembly.Instance> {
-    debug(`instantiateTbsFile:+ filePath=${filePath}`);
-
-    const tempFile = "./tmp1.wasm";
-
-    // Compile the to the temporary outfile
-    await tcCompile(filePath, tempFile);
-            
-    // Instantiate the file
-    let instance = instantiateFile(tempFile);
-    
-    // Remove the temporary outFile
-    let unlinkResult = await unlinkAsync(tempFile);
-    if (unlinkResult.err) {
-        debug(`instantiateTbsFile:- error unlink ${tempFile} result=${JSON.stringify(unlinkResult)}`);
-    }
-
-    // Remove the temporary outFile.log file
-    const logFile = tempFile + ".log";
-    unlinkResult = await unlinkAsync(logFile);
-    if (unlinkResult.err) {
-        debug(`instantiateTbsFile:- error ${logFile} result=${JSON.stringify(unlinkResult)}`);
-    }
-
-    debug(`instantiateTbsFile:- filePath=${filePath} instance=${instance}`);
-    return instance;
-}
 
 @TestFixture("TurboScript tests")
 export class TurboScriptTests {
@@ -116,6 +65,9 @@ export class TurboScriptTests {
         debug("teardown:-");
     }
 
+    /**
+     * An example synchronous test which adds two numbers using TypeScript
+     */
     @TestCase(1, 2, 3)
     @TestCase(-1, 2, 1)
     @TestCase(0, 0, 0)
@@ -123,17 +75,14 @@ export class TurboScriptTests {
     public testAddTwoTypeScript(val1: number, val2: number, expectedResult: number) {
         debug("testAddTwoTypeScript:+");
 
-        try {
-          let result = val1 + val2; //this.instance.exports.addTwo(val1, val2);
-          Expect(result).toBe(expectedResult);
-        } catch (e) {
-          debug(`testAddTwoTypeScript caught e=${e}`);
-          Expect(false).toBe(true); // Always fail.
-        }
+        Expect(val1 + val2).toBe(expectedResult);
 
         debug("testAddTwoTypeScript:-");
     }
 
+    /**
+     * Test TurboScript can add numbers
+     */
     @TestCase(1, 2, 3)
     @TestCase(-1, 2, 1)
     @TestCase(0, 0, 0)
@@ -146,16 +95,14 @@ export class TurboScriptTests {
             this.addTwoInst = await instantiateTbsFile("./tests/addTwo.tbs");
         }
 
-        try {
-            let result = this.addTwoInst.exports.addTwo1(val1, val2);
-            debug(`testAddTwo: result=${result}`);
-            Expect(result).toBe(expectedResult);
-        } catch (e) {
-            debug(`testAddTwo caught e=${e}`);
-            Expect(false).toBe(true); // Always fail.
-        }
+        // Calculate the sum using addTwo1
+        let sum;
+        Expect(() => {
+            sum = this.addTwoInst.exports.addTwo1(val1, val2)
+        }).not.toThrow();
+        Expect(sum).toBe(expectedResult);
 
-        debug("testAddTwo:-");
+        debug(`testAddTwo:- sum=${sum}`);
     }
     private addTwoInst: WebAssembly.Instance; // Used only by testAddTwo
 }
