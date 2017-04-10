71b7697eed99971bea7d72356d9e17466899062b
Nidin Vinayakan <nidinthb@gmail.com>
2017-04-11 00:54:05 +0200

    added windows node installer

--- scripts/install-node8.ts
+++ scripts/install-node8.ts
@@ -7,47 +7,105 @@ interface Nightly {
     date: string;
 }
 
-const nightlyBaseUrl:string = "http://nodejs.org/download/nightly/";
+const nightlyBaseUrl: string = "http://nodejs.org/download/nightly/";
 const request = require('request');
 const fs = require('fs');
 const exec = require('child_process').exec;
+const installerName: string = `node-v8${getPlatform()}`;
+let fileName:string = null;
 
-request.get(nightlyBaseUrl + "index.json", function (error, response, body:string) {
+request.get(nightlyBaseUrl + "index.json", function (error, response, body: string) {
     let nightlyInfo: Nightly[] = JSON.parse(body);
     let downloadUrl = generateDownloadUrl(nightlyInfo);
-
-    if(downloadUrl) {
-        startDownload();
+    if (downloadUrl) {
+        startDownload(downloadUrl);
     }
 });
 
 function generateDownloadUrl(nightlyInfo: Nightly[]): string {
     //Just take first entry, assuming it is the latest.
     let latest: Nightly = nightlyInfo[0];
-    return nightlyBaseUrl + latest.version + getPlatform();
+    fileName = "node-" + latest.version + getPlatform();
+    return nightlyBaseUrl + latest.version + "/" + fileName;
 }
 
-function getPlatform():string {
+function getPlatform(): string {
     let os = process.platform;
 
     switch (os) {
-        case "win32": return "-win-x64.zip";
-        case "darwin": return "-darwin-x64.tar.xz";
-        case "linux": return "-linux-x64.tar.xz";
+        case "win32":
+            return "-win-x64.7z";
+        case "darwin":
+            return "-darwin-x64.tar.xz";
+        case "linux":
+            return "-linux-x64.tar.xz";
     }
 }
 
-function startDownload() {
+function startDownload(url) {
+    console.log(`Downloading ${url}`);
+    request
+        .get(url)
+        .on('error', err => {
+            console.log(err)
+        })
+        .on('end', () => {
+            console.log("Done");
+            let os = process.platform;
 
+            switch (os) {
+                case "win32":
+                    return installWindows();
+                case "darwin":
+                    return installMac();
+                case "linux":
+                    return installLinux();
+            }
+        })
+        .pipe(fs.createWriteStream(installerName));
 }
 
-function install () {
-    exec('-s -o node-v8.tar.xz && tar -xJf node-v8.tar.xz -C nodev8 --strip-components 1 && rm node-v8.tar.xz', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
+function installWindows() {
+    exec('7z x node-v8-win-x64.7z', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        exec(`move ${fileName.replace(".7z", "")} node-v8`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+        });
+    });
+}
+
+function installMac() {
+    exec('tar -xf node-v8-darwin-x64.tar.xz', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        exec(`move ${fileName.replace(".tar.xz", "")} node-v8`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+        });
+    });
+}
+
+function installLinux() {
+    exec('tar -xf node-v8-darwin-x64.tar.xz', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        exec(`move ${fileName.replace(".tar.xz", "")} node-v8`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+        });
     });
 }
