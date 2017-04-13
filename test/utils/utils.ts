3de6c9c2c18bd93f218685260391ede73c71eb6c
Nidin Vinayakan <nidinthb@gmail.com>
2017-04-13 06:20:28 +0200

    sing off

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,9 +1,19 @@
 import * as fs from "fs";
 import * as child from "child_process";
 
+
+//Windows spawn work around
+let spawnSync = child.spawnSync;
+if (process.platform === 'win32') {
+    spawnSync = function(cmd, args) {
+        return child.spawnSync('cmd', ['/s', '/c', cmd].concat(args));
+    }
+}
+
 export function getTurboInstance(sourcePath: string): WebAssembly.Instance {
     const outputFile = sourcePath.replace(".tbs", ".wasm");
-    child.spawnSync('tc', [sourcePath, '--out', outputFile], {stdio: "inherit"});
+    let result = child.spawnSync('../bin/tc', [sourcePath, '--out', outputFile], {stdio: "inherit"});
+    console.log(result.error);
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
     return new WebAssembly.Instance(mod);
