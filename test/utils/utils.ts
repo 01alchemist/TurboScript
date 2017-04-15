694cca1e7ede6baa185ad3cae121f29a7f3c728f
Nidin Vinayakan <01@01alchemist.com>
2017-04-15 11:45:46 +0200

    Debug compile result

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -3,7 +3,8 @@ import * as child from "child_process";
 
 export function getWasmInstance(sourcePath: string, outputFile?:string): WebAssembly.Instance {
     outputFile = outputFile || sourcePath.replace(".tbs", ".wasm");
-    child.spawnSync('../bin/tc', [sourcePath, '--out', outputFile]);
+    let result = child.spawnSync('../bin/tc', [sourcePath, '--out', outputFile]);
+    console.log(result);
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
     return new WebAssembly.Instance(mod);
