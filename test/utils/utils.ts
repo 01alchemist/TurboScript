f1c31cc33b70870a87d4b07476fa5db35daa5025
Nidin Vinayakan <01@01alchemist.com>
2017-04-15 08:44:57 +0200

    tc path changed to ../bin/tc

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -3,7 +3,7 @@ import * as child from "child_process";
 
 export function getWasmInstance(sourcePath: string, outputFile?:string): WebAssembly.Instance {
     outputFile = outputFile || sourcePath.replace(".tbs", ".wasm");
-    child.spawnSync('tc', [sourcePath, '--out', outputFile], {stdio: "inherit"});
+    child.spawnSync('../bin/tc', [sourcePath, '--out', outputFile], {stdio: "inherit"});
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
     return new WebAssembly.Instance(mod);
