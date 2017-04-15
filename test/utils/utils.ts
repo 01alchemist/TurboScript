c056f32e26b562bb501195a0d32c3da0f005899f
Nidin Vinayakan <01@01alchemist.com>
2017-04-15 11:50:34 +0200

    Compiler path updated

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,9 +1,10 @@
 import * as fs from "fs";
 import * as child from "child_process";
+import * as path from "path";
 
 export function getWasmInstance(sourcePath: string, outputFile?:string): WebAssembly.Instance {
     outputFile = outputFile || sourcePath.replace(".tbs", ".wasm");
-    let result = child.spawnSync('../bin/tc', [sourcePath, '--out', outputFile]);
+    let result = child.spawnSync(path.join(__dirname, '../../bin/tc'), [sourcePath, '--out', outputFile]);
     console.log(result);
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
