2d9ac3ad073574705b03f42d64faf0abf36edb77
Nidin Vinayakan <01@01alchemist.com>
2017-04-15 08:37:21 +0200

    Jest test framework configured

--- test/bootstrap.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Nidin Vinayakan on 12/04/17.
- */
-require('ts-node/register');
-require("./addTwo-src");
-
--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as child from "child_process";
 
-export function getTurboInstance(sourcePath: string): WebAssembly.Instance {
-    const outputFile = sourcePath.replace(".tbs", ".wasm");
+export function getWasmInstance(sourcePath: string, outputFile?:string): WebAssembly.Instance {
+    outputFile = outputFile || sourcePath.replace(".tbs", ".wasm");
     child.spawnSync('tc', [sourcePath, '--out', outputFile], {stdio: "inherit"});
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
