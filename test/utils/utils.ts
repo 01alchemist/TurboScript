aa8a178f96a6b2eeef84634d30cf32db64a3cad6
Nidin Vinayakan <01@01alchemist.com>
2017-04-15 11:53:12 +0200

    Build is stable so debug log removed

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -5,7 +5,6 @@ import * as path from "path";
 export function getWasmInstance(sourcePath: string, outputFile?:string): WebAssembly.Instance {
     outputFile = outputFile || sourcePath.replace(".tbs", ".wasm");
     let result = child.spawnSync(path.join(__dirname, '../../bin/tc'), [sourcePath, '--out', outputFile]);
-    console.log(result);
     const data = fs.readFileSync(outputFile);
     const mod = new WebAssembly.Module(data);
     return new WebAssembly.Instance(mod);
