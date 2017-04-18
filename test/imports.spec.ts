439bd97f22a0dcd114254f21f3cdd0cd05e96271
Wink Saville <wink@saville.com>
2017-04-18 13:00:35 -0700

    Add precision on tests using toBeCloseTo.
    According to [this](https://facebook.github.io/jest/docs/expect.html#tobeclosetonumber-numdigits)
    the default precision is 2 that seems to few, so using 10.
    
    Also, added the `precision` test to validate the test.

--- test/imports.spec.ts
+++ test/imports.spec.ts
@@ -3,6 +3,8 @@ import { getWasmInstanceSync } from "./utils/utils";
 
 let instance: WebAssembly.Instance;
 const TwoPI = 2 * Math.PI;
+const precision = 10; // Number of digits of precision for toBeCloseTo
+const precisionValue = parseFloat(`1.0e-${precision}`);
 
 test("it should compile without error", () => {
     instance = getWasmInstanceSync(
@@ -12,12 +14,22 @@ test("it should compile without error", () => {
     expect(instance).toBeDefined();
 })
 
+test("precision", () => {
+    const value: number = Math.random() * TwoPI;
+    const expected: number = Math.sin(value);
+    const expectedFail: number = expected + precisionValue;
+
+    const result: number = instance.exports.sin(value);
+    expect(result).toBeCloseTo(expected, precision);
+    expect(result).not.toBeCloseTo(expectedFail, precision);
+});
+
 test("it should import sin function from javascript", () => {
     const value: number = Math.random() * TwoPI;
     const expected: number = Math.sin(value);
 
     const result: number = instance.exports.sin(value);
-    expect(result).toBeCloseTo(expected);
+    expect(result).toBeCloseTo(expected, precision);
 });
 
 test("it should import cos function from javascript", () => {
@@ -25,7 +37,7 @@ test("it should import cos function from javascript", () => {
     const expected: number = Math.cos(value);
 
     const result: number = instance.exports.cos(value);
-    expect(result).toBeCloseTo(expected);
+    expect(result).toBeCloseTo(expected, precision);
 });
 
 test("it should import tan function from javascript", () => {
@@ -33,7 +45,7 @@ test("it should import tan function from javascript", () => {
     const expected: number = Math.tan(value);
 
     const result: number = instance.exports.tan(value);
-    expect(result).toBeCloseTo(expected);
+    expect(result).toBeCloseTo(expected, precision);
 });
 
 test("it should import abs function from javascript", () => {
@@ -41,5 +53,5 @@ test("it should import abs function from javascript", () => {
     const expected: number = Math.fround(Math.abs(value)); //WASM abs is 32bit float
 
     const result: number = instance.exports.abs(value);
-    expect(result).toBeCloseTo(expected);
+    expect(result).toBeCloseTo(expected, precision);
 });
